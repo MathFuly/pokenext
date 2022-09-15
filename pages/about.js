ebbdaac590c856efbfd8e2d90cdba3167f38b597
MatFuly <matheus.fuly.ferreira1@gmail.com>
2022-09-15 20:51:54 -0300

    Enviando projeto

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+import styles from "../styles/About.module.css";
+
+export default function About() {
+  return (
+    <div className={styles.about}>
+      <h1>Sobre o projeto</h1>
+      <p>PokeNext é um App construído em Next.js para consultar Pokémons.</p>
+      <Image src="/images/155.png" width="300" height="300" alt="Cyndaquil" />
+    </div>
+  );
+}
